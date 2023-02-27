8ec0404f812af7af5c8b7cd209c0335c7396ff01
romanpepper <wupsen1703@mail.ru>
2023-02-27 20:03:04 +0300

    MVC + v0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
+public class Main {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        WindowApplication windowApplication = new WindowApplication();
     }
 }
